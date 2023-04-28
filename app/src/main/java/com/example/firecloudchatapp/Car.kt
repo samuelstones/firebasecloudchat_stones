242d4a8a22b23b1269ce0c7ee278c977bd7af8fd
kiddo-byte <seanorina@gmail.com>
2023-04-28 13:17:36 +0300

    kotlin project

---
+++ app/src/main/java/com/example/firecloudchatapp/Car.kt
@@ -0,0 +1,36 @@
+package com.example.firecloudchatapp
+
+import java.lang.reflect.Constructor
+
+class Car {
+
+    var make:String? = null
+    var model:String? = null
+    var price:String? = null
+
+
+    constructor(make:String, model:String, price:String){
+
+        this.make = make
+        this.model = model
+        this.price = price
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/firecloudchatapp/MainActivity.kt
+++ app/src/main/java/com/example/firecloudchatapp/MainActivity.kt
@@ -9,8 +9,13 @@ import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var datatext: EditText
-    private lateinit var savebutton: Button
+    private lateinit var edtcarmake: EditText
+    private lateinit var edtcarmodel: EditText
+    private lateinit var edtcarprice: EditText
+
+    private lateinit var carphoto: Button
+    private lateinit var cardata: Button
+    private lateinit var viewcars: Button
 
 
 
@@ -18,26 +23,77 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        datatext = findViewById(R.id.edtnamesavedata)
-        savebutton = findViewById(R.id.btnsavedata)
+
+        edtcarmake = findViewById(R.id.editcarmake)
+        edtcarmodel = findViewById(R.id.editcarmodel)
+        edtcarprice = findViewById(R.id.editcarprice)
+
+        carphoto = findViewById(R.id.btncarphoto)
+        cardata = findViewById(R.id.btncardata)
+        viewcars = findViewById(R.id.btnviewuploadcars)
+
+
+
 
         //initialize
         var database = FirebaseDatabase.getInstance()
-        var databaseRef = database.reference
+        var databaseref = database.getReference("cars")
 
 
+        carphoto.setOnClickListener {
 
-        savebutton.setOnClickListener {
-
-
-            var user_data =datatext.text.toString().trim()
-            //Toast.makeText(this, "user_data", Toast.LENGTH_SHORT).show()
-
-            databaseRef.setValue(user_data)
+        }
 
 
 
 
+        cardata.setOnClickListener {
+
+            var make = edtcarmake.text.toString().trim()
+            var model = edtcarmodel.text.toString().trim()
+            var price = edtcarprice.text.toString().trim()
+
+            //validate inputs
+
+            if (make.isEmpty() || model.isEmpty() || price.isEmpty()) {
+                Toast.makeText(this, "one of the fields is empty :(", Toast.LENGTH_SHORT).show()
+            } else {
+                //saving info to firebase db
+                var usercar = Car(make,model,price)
+
+                var ref = FirebaseDatabase.getInstance().getReference().child("cars")
+
+                ref.setValue(usercar).addOnCompleteListener{
+
+                    if (it.isSuccessful){
+                        Toast.makeText(this,"Car Data Uploaded Successfully",
+                            Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this,"Failed tO Save Car Info",
+                            Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        viewcars.setOnClickListener {
 
         }
 
